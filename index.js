5eaaa7502862eea345bbf8be6dec1f9562760fdd
Sta11k <Alexandiel1984@gmail.com>
2021-08-11 16:25:45 +0300

    start-1

--- index.js
+++ index.js
@@ -8,5 +8,44 @@ const colors = [
 ];
 
 const randomIntegerFromInterval = (min, max) => {
+   min = 0;
+   max = 5;
   return Math.floor(Math.random() * (max - min + 1) + min);
+ 
 };
+
+const startRef = document.querySelector('button[data-action="start"]');
+const stopRef = document.querySelector('button[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+
+
+function switchColor(e) {
+  
+  
+  const cologBg = colors[`${randomIntegerFromInterval()}`];
+  bodyRef.style.backgroundColor = `${cologBg}`;
+ 
+  startRef.setAttribute("disabled", "true");
+
+
+ // startRef.classList.add("pointer-events");
+
+ 
+  ;}
+
+
+startRef.addEventListener('click', switchColor);
+// startRef.addEventListener('click', event => {
+//   event.target = true;
+
+//     const randomNumber = randomIntegerFromInterval();
+// console.log();
+// console.log(valueAtIndex1);
+// console.log(colorBg);
+// });
+
+// startRef.addEventListener('click',  );
+
+
+
+
